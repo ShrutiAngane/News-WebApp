d206f21f61a849b30035556b5f0da0d1042f0c10
Shruti Angane <shrutiangane628@gmail.com>
2024-01-18 10:48:15 +0530

    added the disable button feature for pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -5,20 +5,23 @@ import '../index.css'
 
 const Pagination = (props) => {
     const page_size=props.page_size<=10?1:props.page_size<=50?Math.ceil(props.page_size/10):5;
+    console.log(page_size);
     const[indexno,setIndex]=useState(1)
+    console.log('index is :',indexno)
     function paginate(){
         props.setpage(props.page)
     }
   return (
     <section className="flex justify-center gap-[10px]">
       <button
-        className={`${indexno==1?'disabled':''} text-[10px] w-[25px] md:w-[40px] md:h-[150px] md:mx-5 md:text-[16px]`}
+        className={`text-[10px] w-[25px] md:w-[40px] md:h-[150px] md:mx-5 md:text-[16px]`}
+        disabled={indexno==1?true:false}
         value={"Previous"}
         onClick={() => {
           setIndex((prev)=>prev+1)
           props.setpage(props.page)}}
       >
-        <img src={previous} className={`${indexno===1?'opacity-25':''}`}/>
+        <img src={previous} className={`${indexno==1?'opacity-25':''}`}/>
         Previous
       </button>
       <div className="flex justify-center items-center mt-[-5px] gap-1">
@@ -39,14 +42,15 @@ const Pagination = (props) => {
         })}
       </div>
       <button
-        className="text-[10px] w-[25px] md:w-[40px] md:h-[150px] md:text-[16px]"
+        className={`${indexno==page_size?'disabled':''} text-[10px] w-[25px] md:w-[40px] md:h-[150px] md:text-[16px]`}
+        disabled={indexno==page_size?true:false}
         value={"Next"}
         onClick={() => {
           props.setpage(props.page)
           setIndex((prev)=>prev+1)
         }}
       >
-        <img src={Next} className={`${indexno===page_size?'opacity-25':''}`}/>
+        <img src={Next} className={`${indexno==page_size?'opacity-25':''}`}/>
         Next
       </button>
     </section>
